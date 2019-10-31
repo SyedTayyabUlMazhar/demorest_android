526e0db71547ae56de8296a1890a0ef13565799c
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2019-10-31 22:39:53 +0500

    Replaced employee parsing with student parsing.

--- app/src/main/java/com/example/demorest_android/MainActivity.java
+++ app/src/main/java/com/example/demorest_android/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.demorest_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -14,55 +17,67 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    private static final String TAG = "MainActivity";
+    private Context context;
 
     RequestQueue requestQueue;
     Button btn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
 
         requestQueue = Volley.newRequestQueue(this);
 
         btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 parseJson();
             }
         });
     }
 
-    public void parseJson() {
+    public void parseJson()
+    {
         String url = ((EditText) findViewById(R.id.textView)).getText().toString();
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>()
+                {
                     @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-//                            JSONObject jsonObject = response.getJSONObject("employee");
-//                            response.get
-                            int id = response.getInt("id");
-                            String firstName = response.getString("firstName");
+                    public void onResponse(JSONObject response)
+                    {
+                        TextView result = findViewById(R.id.textView_result);
 
-                            TextView result = findViewById(R.id.textView_result);
-                            result.setText("Result : " + id + " " + firstName);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                        Gson gson = new Gson();
+                        Student student = gson.fromJson(response.toString(), Student.class);
+
+                        result.setText(student.toString());
+                        Toast.makeText(context, student.toString(), Toast.LENGTH_LONG).show();
                     }
                 },
-                new Response.ErrorListener() {
+                new Response.ErrorListener()
+                {
                     @Override
-                    public void onErrorResponse(VolleyError error) {
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Log.d(TAG, "Failed : " + error.getMessage());
                         error.printStackTrace();
                     }
                 }
